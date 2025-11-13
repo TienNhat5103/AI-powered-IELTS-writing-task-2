d4512d9de50559c0452b5728f94792bc0661c774
TienNhat5103 <nhatkontum123@gmail.com>
2025-11-13 19:35:15 +0700

    Initial commit

---
+++ backend/Bert_setup.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from transformers import  BertTokenizer
+import numpy as np
+import torch
+import os
+import joblib
+from huggingface_hub import login, hf_hub_download
+from dotenv import load_dotenv
+from BERT_Model import BERTWithExtraFeature, round_to_nearest_half_np, preprocess_inputs_pt
+# from transformers import AutoConfig
+load_dotenv()
+login(os.getenv("IELTS_HUGGINGFACE_API_KEY"))
+bert_tokenizer = BertTokenizer.from_pretrained("Tiennhat123/IELTS_BERT_FINETUNE")
+model = BERTWithExtraFeature()
+device = "cpu"
+model_path = hf_hub_download(
+    repo_id="Tiennhat123/IELTS_BERT_FINETUNE",
+    filename="pytorch_model.bin"
+)
+model.load_state_dict(torch.load(model_path, map_location="cpu"))
+scaler_path = hf_hub_download(
+    repo_id="Tiennhat123/IELTS_BERT_FINETUNE",
+    filename="scaler.pkl"
+)
+scaler = joblib.load(scaler_path)
+
+def get_overall_score(question, answer):
+    # preprocess the input
+    input_ids, attention_mask, extra_number = preprocess_inputs_pt(question, answer, bert_tokenizer, scaler, device, max_length=512)
+
+    model.eval()  # Set the model to evaluation mode
+    with torch.no_grad():  # No gradient computation during testing
+        output = model(input_ids, attention_mask, extra_number)
+        output = output.cpu().numpy()
+        score = round_to_nearest_half_np(output, method='nearest')
+        
+    return score[0][0]
---
+++ model.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from transformers import BertModel
+class BERTWithExtraFeature(nn.Module):
+    def __init__(self, pretrained_model_name='bert-base-uncased', dropout_prob=0.2, num_trainable_layers=1):
+        super(BERTWithExtraFeature, self).__init__()
+        self.bert = BertModel.from_pretrained(pretrained_model_name)
+
+        # Freeze all layers in the BERT model
+        for param in self.bert.parameters():
+            param.requires_grad = False
+
+        # Unfreeze the last `num_trainable_layers` layers
+        for layer in self.bert.encoder.layer[-num_trainable_layers:]:
+            for param in layer.parameters():
+                param.requires_grad = True
+
+        # BatchNorm for numerical input
+        self.num_feature_norm = nn.BatchNorm1d(1)
+
+        # Combined input size: 768 (BERT) + 1 (extra num)
+        self.concat_input_dim = 768 + 1
+
+        # New feed-forward layer stack
+        self.fc0 = nn.Linear(self.concat_input_dim, 512)
+        self.relu0 = nn.ReLU()
+        self.fc1 = nn.Linear(512, 256)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(256, 128)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(128, 64)
+        self.relu3 = nn.ReLU()
+        self.output_layer = nn.Linear(64, 1)
+
+    def forward(self, input_ids, attention_mask, extra_number):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs.pooler_output
+
+        if extra_number.dim() == 1:
+            extra_number = extra_number.unsqueeze(1)
+        normalized_num = self.num_feature_norm(extra_number)
+
+        concat = torch.cat((pooled_output, normalized_num), dim=1)
+
+        x = self.fc0(concat)
+        x = self.relu0(x)
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.relu3(x)
+        
+        output = self.output_layer(x)
+
+        return output
